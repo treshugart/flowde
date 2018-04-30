4a45d547df4947769e42e8a5d5f6182e8639bd33
Trey Shugart <treshugart@gmail.com>
2018-04-30 14:10:16 +1000

    Fix error reporting and use glow instead.

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 #!/usr/bin/env node
 
-const { spawnSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+const { spawnSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
-const babel = require('babel-register')({
+const babel = require("babel-register")({
   presets: [
-    [require('babel-preset-env'), {
-      targets: {
-        node: true
+    [
+      require("babel-preset-env"),
+      {
+        targets: {
+          node: true
+        }
       }
-    }],
-    require('babel-preset-react')
+    ],
+    require("babel-preset-flow"),
+    require("babel-preset-react"),
+    require("babel-preset-stage-0")
   ]
 });
-const [ node, flowde, file ] = process.argv;
-const run = (...args) => spawnSync(args.shift(), args).output[1].toString();
+const [node, flowde, file] = process.argv;
+const run = (...args) => spawnSync(args.shift(), args);
 
 const cwd = process.cwd();
 const filePath = path.resolve(file);
-const flowPath = path.join(__dirname, 'node_modules', '.bin', 'flow');
-const flowConfigPath = path.join(cwd, '.flowconfig');
+const flowPath = path.join(__dirname, "node_modules", ".bin", "glow");
+const flowConfigPath = path.join(cwd, ".flowconfig");
 
 let check;
 
 if (fs.existsSync(flowConfigPath)) {
   check = run(flowPath, file);
 } else {
-  run(flowPath, 'init');
+  run(flowPath, "init");
   check = run(flowPath, file);
-  run('rm', '-rf', flowConfigPath);
+  run("rm", "-rf", flowConfigPath);
 }
 
-if (check.indexOf('No errors!') === -1) {
-  throw new Error(check);
-}
+const err = check.stderr.toString();
 
-require(filePath);
+// Glow reports success via stderr, so we must check to see if it was
+// successful.
+if (err.indexOf("success") === -1) {
+  console.log(err);
+} else {
+  require(filePath);
+}
